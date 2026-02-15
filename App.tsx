277972c77f326d9be7be535cdb9d22318e761e97
felixvarun-episode <felixvarun.episode@gmail.com>
2026-02-15 06:53:46 +0530

    patches

--- App.tsx
+++ App.tsx
@@ -14,17 +14,20 @@ import {
 import AppStack from './src/navigation';
 import { Provider } from 'react-redux';
 import { store } from './src/redux/store';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
+     <GestureHandlerRootView style={{flex: 1}}>
     <Provider store={store}>
       <SafeAreaProvider>
         {/* <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} /> */}
         <AppContent />
       </SafeAreaProvider>
     </Provider>
+    </GestureHandlerRootView>
   );
 }
 
